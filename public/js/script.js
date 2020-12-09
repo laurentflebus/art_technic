c25cf6fafbc0f43fb59f800731c5ed0a3ecd0b63
Laurent <laurentflebus@gmail.com>
2020-12-09 15:36:59 +0100

    Externalisé le code jQuery script.js + total ttc

---
+++ public/js/script.js
@@ -0,0 +1,115 @@
+
+jQuery(function($){
+
+    // Nombre de poste max
+    var max = 10;
+    var cptPoste = 1;
+    var cPoste = '1';
+    // Évenement lorsqu'une touche est relachée dans le champs code barre
+    $('#codebarre1').keyup(function(e){
+        var codebarre = $('#codebarre1').val();
+        // crée une instance de XmlHttpRequest
+        // permet d'envoyer une requête HTTP 
+        // Appel AJAX en JQuery                   
+        $.ajax({                        
+            url : '/ajax', // fichier cible coté serveur, script qui récupère les infos du poste de vente
+            type: 'GET', // Type de la requête HTTP
+            data: {'codebarre': codebarre}, // passe la variable codebarre issue du formulaire
+            datatype: 'json', // type de données à recevoir      
+            // si l'appel AJAX a réussi
+            success: function(data) {
+                // code pour gérer le retour de l'appel AJAX
+                console.log(data);
+                $('#prixtvac'+ $('#nbPoste').val()).val(data[0].prix_unitaire);                            
+                $('#numeroposte'+ $('#nbPoste').val()).val(data[0].numero);
+                $('#intituleposte'+ $('#nbPoste').val()).val(data[0].intitule);
+
+                
+                var prixunitaire = data[0].prix_unitaire;
+                var taux = data[1].taux;
+                var prixhtva = prixunitaire * (1-(taux/100));                           
+                $('#prixhtva'+ $('#nbPoste').val()).val(prixhtva.toFixed(2));
+
+                console.log($('#nbPoste').val());    
+            },    
+        });
+    });
+    // Évenement lorsqu'une touche est relachée dans le champs quantite
+    $('#quantite1').keyup(function(e){
+        var quantite = $('#quantite1').val();
+        var codebarre = $('#codebarre1').val();
+        $.ajax({                        
+            url : '/ajax', // fichier cible coté serveur, script qui récupère les infos du poste de vente
+            type: 'GET', // Type de la requête HTTP
+            data: {'codebarre': codebarre}, // passe la variable codebarre issue du formulaire
+            datatype: 'json', // type de données à recevoir      
+            // si l'appel AJAX a réussi
+            success: function(data) {
+                // code pour gérer le retour de l'appel AJAX
+                console.log(data);
+                var prixunitaire = data[0].prix_unitaire;
+                var total = quantite * prixunitaire;
+
+                $('#totalttca'+ $('#nbPoste').val()).val(total.toFixed(2));
+                $('#totalttc'+ $('#nbPoste').val()).val(total.toFixed(2));
+                $('#quantite'+$('#nbPoste').val()).val(quantite);
+
+                // Calcul du total TTC
+                var totalttc = 0;
+                for (let i = 1; i <= cPoste; i++) {
+                    totalttc += Number($('#totalttca'+i).val());
+                }
+                $('#totalttc').val(totalttc.toFixed(2));
+            },           
+        });        
+    });
+    
+    $('#ajouterposte').click(function(e) {
+
+        if (cptPoste < max) {
+            // incremente le compteur et transforme en chaine de caractère
+            cptPoste++;
+            cPoste = cptPoste.toString();
+            // clone la div avec tous les éléments du poste
+            var clone = $('#clone').clone();
+
+            // ajoute à une div vide le clone
+            $('#bloc').append(clone);
+            // modifie les attributs id et name des input codebarre, numeroposte, intituleposte, quantite, prixtvac, prixhtva, totalttca
+            $('#bloc #codebarre1').attr('id', 'codebarre'+cPoste);
+            $('#bloc #codebarre1').attr('name', 'codebarre'+cPoste);
+            $('#bloc #numeroposte1').attr('id', 'numeroposte'+cPoste);
+            $('#bloc #numeroposte1').attr('name', 'numeroposte'+cPoste);
+            $('#bloc #intituleposte1').attr('id', 'intituleposte'+cPoste);
+            $('#bloc #intituleposte1').attr('name', 'intituleposte'+cPoste);
+            $('#bloc #quantite1').attr('id', 'quantite'+cPoste);
+            $('#bloc #quantite1').attr('name', 'quantite'+cPoste);
+            $('#bloc #prixtvac1').attr('id', 'prixtvac'+cPoste);
+            $('#bloc #prixtvac1').attr('name', 'prixtvac'+cPoste);
+            $('#bloc #prixhtva1').attr('id', 'prixhtva'+cPoste);
+            $('#bloc #prixhtva1').attr('name', 'prixhtva'+cPoste);
+            $('#bloc #totalttca1').attr('id', 'totalttca'+cPoste);
+            $('#bloc #totalttca1').attr('id', 'totalttca'+cPoste);
+
+            // vide les nouveaux champs
+            $('#codebarre'+cPoste).val('');
+            $('#numeroposte'+cPoste).val('');
+            $('#intituleposte'+cPoste).val('');
+            $('#quantite'+cPoste).val('');
+            $('#prixtvac'+cPoste).val('');
+            $('#prixhtva'+cPoste).val('');
+            $('#totalttca'+cPoste).val('');
+                                    
+            // change la valeur de l'input caché avec la derrière valeur de cPoste (nombre de poste).
+            $('#nbPoste').val(cPoste);
+        }
+        console.log(cPoste);
+
+    });
+
+    // événement menu aside
+    $("#menu-toggle").click(function(e) {
+        e.preventDefault(); // annule l'action du div id=menu-toggle
+        $("#wrapper").toggleClass("toggled");
+    });
+});
